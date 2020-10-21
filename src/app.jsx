7c98a90ae76b4f2724a651049f350f9422c89ce0
KimJeongmin-sys <hikjmjuly23@gmail.com>
2020-10-22 01:10:14 +0900

    firebase add & auth setting

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./app.css";
+import Login from "./components/login/login";
+
+function App({ authService }) {
+  return <Login authService={authService} />;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.module.css";
+import App from "./app";
+import AuthService from "./service/auth_service";
+const authService = new AuthService();
+ReactDOM.render(
+  <App authService={authService} />,
+  document.getElementById("root")
+);
